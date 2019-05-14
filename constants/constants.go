f8798cc1a3a41c7a20b9f6ce6ebc4fad31bc8c51
gky360 <gky360@gmail.com>
2019-05-15 02:48:44 +0900

    upgrade version to v0.1.3

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,6 @@
 package constants
 
-const Version = "v0.1.2"
+const Version = "v0.1.3"
 const AtCoderHost = "atcoder.jp"
 const DropboxHost = "www.dropbox.com"
 const AtcoderTestcasesRootPath = "/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa"
